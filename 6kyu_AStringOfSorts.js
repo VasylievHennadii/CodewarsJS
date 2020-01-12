40590f9ed93dd7602ce3ad6e88cc39f1bdc9a42b
VasylievHennadii <vga.kha@gmail.com>
2020-01-12 14:05:17 +0200

    A string of sorts

---
+++ 6kyu_AStringOfSorts.js
@@ -0,0 +1,30 @@
+// Define a method that accepts 2 strings as parameters. The method returns the first string sorted by the second.
+
+// sortString("foos", "of")       => "oofs"
+// sortString("string", "gnirts") => "gnirts"
+// sortString("banana", "abn")    => "aaabnn"
+// To elaborate, the second string defines the ordering. It is possible that in the second string characters repeat, so you should remove repeating characters, leaving only the first occurrence.
+
+// Any character in the first string that does not appear in the second string should be sorted to the end of the result in original order.
+
+// first solution
+function sortString(string,ordering) {
+    let arr = string.split(""), 
+    s = "";
+ for (let o of ordering )
+   while (arr.includes(o))
+     s += arr.splice( arr.indexOf(o), 1 )[0];
+ s += arr.join("");
+ return s;
+}
+
+// second solution
+function sortString(string,ordering) {
+    let ordered = ordering.split('')
+    return string
+      .split('')
+      .filter(a => ordered.includes(a))
+      .sort((a, b) => { return ordered.indexOf(a) - ordered.indexOf(b) })
+      .concat(string.split('').filter(a => !ordered.includes(a)))
+      .join('')
+}
